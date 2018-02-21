8c9e74101b206838c86409892e9afcd453eeda6f
Ernests Elksnis <eelksnis@yahoo.com>
2018-02-21 19:12:12 +0100

    Initial commit

---
+++ src/google-places-api.cr
@@ -0,0 +1,6 @@
+require "./google-places-api/*"
+
+# TODO: Write documentation for `Google::Places::Api`
+module Google::Places::Api
+  # TODO: Put your code here
+end
---
+++ src/google-places-api/version.cr
@@ -0,0 +1,3 @@
+module Google::Places::Api
+  VERSION = "0.1.0"
+end
