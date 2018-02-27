cfe6a3fb9d4d13c23be318fb401a16fd3f1634ea
Ernests Elksnis <eelksnis@yahoo.com>
2018-02-27 19:47:07 +0100

    To json

--- spec/autocomplete_spec.cr
+++ spec/autocomplete_spec.cr
@@ -1,5 +1,8 @@
 require "./spec_helper"
 
 describe GooglePlaces::Api::Autocomplete do
-  
+  it "should return " do
+    client = GooglePlaces::Api::Client.new("AIzaSyAduvoZU_b83yb9Ntn-6yuJ79QvIX9anIM", GooglePlaces::Formats::Json, tls = false)
+    client.autocomplete("Riga")
+  end
 end
--- src/google-places/api/autocomplete.cr
+++ src/google-places/api/autocomplete.cr
@@ -6,7 +6,7 @@ module GooglePlaces
       def autocomplete(input : String, options = {} of String => String)
         options = options.merge({"input" => input})
         response = get(Path, options)
-        GooglePlaces::Models::Response.from_json(response)
+        GooglePlaces::Models::Response.from_json(response).to_json
       end
     end
   end
